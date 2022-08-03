11adb9d8bf6f527607f3be2a4a0f822a061d3f5d
yeongi <cbg5255@gmail.com>
2022-08-03 15:37:28 +0900

    layout

--- src/component/note/MyNote.js
+++ src/component/note/MyNote.js
@@ -2,18 +2,19 @@ import React, { useState } from "react";
 import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import parse from "html-react-parser";
-import classses from "./MyNote.module.css";
+import classes from "./MyNote.module.css";
 
 const MyNote = () => {
   const [content, setContents] = useState("");
 
   return (
-    <div className={classses.wrapper}>
+    <div className={classes.wrapper}>
       <ReactQuill
         theme="snow"
         value={content}
         onChange={setContents}
         placeholder="지금부터 써내려 가"
+        className={classes.note}
       />
       <p>{parse(content)}</p>
     </div>
--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -7,12 +7,12 @@ const HomePage = () => {
     <div className={classes["home-wrapper"]}>
       <section className={classes.wrapper}>
         <article>치트시트 생성</article>
-        <article>
+        <article className={classes.note}>
           <h3>게시글 작성</h3>
           <hr />
           <MyNote />
         </article>
-        <article>치트시트 수정</article>
+        <article>치트시트 보기</article>
         <article>알림 설정</article>
       </section>
     </div>
