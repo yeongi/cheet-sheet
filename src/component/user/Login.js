0ab8a141e6ae775f05af53a4179de896e2c440ce
yeongi <cbg5255@gmail.com>
2022-08-04 21:06:18 +0900

    login

--- src/component/user/Login.js
+++ src/component/user/Login.js
@@ -1,9 +1,58 @@
-import React from "react";
+import { TextField, Button } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [username, setName] = useState("");
+  const [password, setPW] = useState("");
+
+  const nameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const passwordChange = (e) => {
+    setPW(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    console.log(username, password);
+  };
+
   return (
     <div>
       <h1>로그인</h1>
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+        onSubmit={submitHandler}
+      >
+        <TextField
+          id="outliend-name"
+          label="User"
+          variant="outlined"
+          value={username}
+          onChange={nameChange}
+        />
+        <br />
+        <TextField
+          id="outliend-name"
+          label="Password"
+          variant="outlined"
+          type="password"
+          value={password}
+          onChange={passwordChange}
+        />
+        <br />
+        <Button type="submit" variant="outlined">
+          제출하기
+        </Button>
+      </Box>
     </div>
   );
 };
--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,19 +1,45 @@
 import React from "react";
 import MyNote from "../component/note/MyNote";
+import Login from "../component/user/Login";
 import classes from "./HomePage.module.css";
+import { useState } from "react";
+import { Button } from "@mui/material";
 
 const HomePage = () => {
+  const [isLoggedIn, setLogIn] = useState(false);
+
+  const loginHandler = (e) => {
+    if (isLoggedIn) {
+      setLogIn(false);
+    } else {
+      setLogIn(true);
+    }
+  };
+
   return (
     <div className={classes["home-wrapper"]}>
       <section className={classes.wrapper}>
-        <article>치트시트 생성</article>
-        <article className={classes.note}>
-          <h3>게시글 작성</h3>
-          <hr />
-          <MyNote />
-        </article>
-        <article>치트시트 보기</article>
-        <article>알림 설정</article>
+        {!isLoggedIn && (
+          <article>
+            <h3>로그인</h3>
+            <hr />
+            <Login />
+          </article>
+        )}
+        {isLoggedIn && (
+          <>
+            <article>치트시트 생성</article>
+            <article className={classes.note}>
+              <h3>게시글 작성</h3>
+              <hr />
+              <MyNote />
+            </article>
+
+            <article>치트시트 보기</article>
+            <article>알림 설정</article>
+          </>
+        )}
+        <Button onClick={loginHandler}>로그인 했다</Button>
       </section>
     </div>
   );
